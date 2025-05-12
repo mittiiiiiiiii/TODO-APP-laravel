661588b48ff05eba217e0d3c19448499b074b5d7
Mitiii <srp5628comust@gmail.com>
2025-05-13 01:18:46 +0900

    feat: aliasを導入

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    resolve: {
+    alias: {
+        '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
