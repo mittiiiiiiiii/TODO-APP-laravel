06bff7d46976ba39fb81676e1826b68d7f9a4890
Mitiii <srp5628comust@gmail.com>
2025-05-13 18:41:33 +0900

    feat: web.phpにRouteを設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\User;
+use App\Models\Task;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +12,29 @@ Route::get('/', function () {
 Route::get('/Preview', function () {
     return Inertia::render('Preview');
 });
+
+Route::get('/register', function () {
+    return Inertia::render('Register');
+})->name('Register');
+
+Route::get('/login', function () {
+    return Inertia::render('Login');
+})->name('Login');
+
+Route::prefix('tasks')->group(function () {
+    Route::get('/tasks', function () {
+        return Inertia::render('Tasks');
+    })->name('Tasks');
+
+    Route::get('/tasks/:id/edit', function () {
+        return Inertia::render('Tasks/[id]/Edit');
+    })->name('Tasks.Edit');
+
+    Route::get('/tasks/new', function () {
+        return Inertia::render('Tasks');
+    })->name('Tasks.New');
+});
+
+Route::get('/profile', function () {
+    return Inertia::render('Profile');
+})->name('Profile');
